2a44e84ee6b8ad3bd2aa73f14817231650f5b574
Piyushshind <piyushshinde.199@gmail.com>
2024-02-25 21:59:43 +0530

    created TS for practice

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var num = 30;
+if (num < 50)
+    ++num;
+console.log(num);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let num = 30;
+if (num < 50)
+    ++num;
+console.log(num);
+
+function domm(doc:number){
+    return doc;
+}
+domm(5);
